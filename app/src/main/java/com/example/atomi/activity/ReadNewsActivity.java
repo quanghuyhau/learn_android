03e5d1c2330a832afc851a0a66f03b5737482d11
Tran Quang Huy <Tran Quang Huy>
2024-08-13 16:58:38 +0700

    fix webview 13/08/2023

---
+++ app/src/main/java/com/example/atomi/activity/ReadNewsActivity.java
@@ -0,0 +1,52 @@
+package com.example.atomi.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.example.atomi.R;
+
+public class ReadNewsActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_news);
+
+        webView  = findViewById(R.id.webView);
+
+        String url = getIntent().getStringExtra("URL");
+
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+
+        webView.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
+
+        webView .loadUrl(url);
+        setContentView(webView );
+
+    }
+    }
--- app/src/main/java/com/example/atomi/retrofit/Adapter.java
+++ app/src/main/java/com/example/atomi/retrofit/Adapter.java
@@ -1,6 +1,7 @@
 package com.example.atomi.retrofit;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.atomi.R;
+import com.example.atomi.activity.ReadNewsActivity;
 
 import java.util.ArrayList;
 
@@ -36,13 +38,24 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder , int position) {
         holder.headlines.setText(modelArrayList.get(position).getTitle());
         holder.mainews.setText(modelArrayList.get(position).getDescription());
         holder.author.setText(modelArrayList.get(position).getAuthor());
         holder.pulishedat.setText(modelArrayList.get(position).getPublishedAt());
-
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int adapterPosition = holder.getAdapterPosition();
+                if (adapterPosition != RecyclerView.NO_POSITION) {
+                    Intent intent = new Intent(context, ReadNewsActivity.class);
+                    intent.putExtra("URL", modelArrayList.get(adapterPosition).getUrl());
+                    context.startActivity(intent);
+                }
+            }
+        });
         Glide.with(context).load(modelArrayList.get(position).getUrlToImage()).into(holder.imageView);
+
     }
 
     @Override
